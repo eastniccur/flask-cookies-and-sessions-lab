cad9e1747c835914ab0720a29d7e01d96210e27d
Zakaria Abdi <zakariaabdisheikh004@gmail.com>
2026-08-16 23:07:06 +0300

    Cookies

--- server/app.py
+++ server/app.py
@@ -27,7 +27,19 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-    pass
+    if 'page_views' not in session:
+        session['page_views'] = 0
+
+    session['page_views'] += 1
+
+    if session['page_views'] > 3:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+
+    article = Article.query.filter_by(id=id).first()
+    if article is None:
+        return jsonify({'message': 'Article not found'}), 404
+
+    return make_response(ArticleSchema().dump(article))
 
 
 if __name__ == '__main__':
